38c555585906a0d6b42ce114888ed764fe313518
coleYab <yabume123@gamil.com>
2024-08-15 22:24:03 +0300

    + added add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+add function with type annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add -> function to add two numbers
+    """
+    return a + b
