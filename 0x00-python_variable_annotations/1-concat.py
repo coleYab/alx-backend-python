120e2fc8a079dfccbf07cdac76c7f4538a7ce464
coleYab <yabume123@gamil.com>
2024-08-16 09:53:11 +0300

    concat string added

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Concatnation functoin for strngs
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat takes two string and return appended string
+    """
+    return f"{str1}{str2}"
