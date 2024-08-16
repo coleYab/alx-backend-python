3160ae497ffbf8363aad794ad56ff173531fcc41
coleYab <yabume123@gamil.com>
2024-08-16 09:58:56 +0300

    + added to_string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for getting the c representation of a number
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str -> takes a float n -> str(n)
+    """
+    return str(n)
