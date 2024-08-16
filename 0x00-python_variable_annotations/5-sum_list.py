58758ce1e2a6721bb2f9fc04d16b9fa201d385d8
coleYab <yabume123@gamil.com>
2024-08-16 12:27:34 +0300

    + added sum lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+sum_lists - > adding float lists to return them in thing.
+"""
+
+
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+    type annotated sum_lists function
+    """
+    return sum(input_list)
