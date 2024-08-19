6058646d5868afcaa92d572effff3640072938d0
coleYab <yabume123@gamil.com>
2024-08-19 17:45:20 +0300

    + created meansure time for the data

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+meansuring the runtime of the wait_n coroutene
+"""
+
+
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_wait: int) -> float:
+    """
+    meansure_time function to meansure the total run time of a function.
+    args:
+        - n: total args.
+        - max_wait: the maximum possible delay.
+    """
+    wait_time = asyncio.run(wait_n(n, max_wait))
+    return sum(wait_time) / len(wait_time)
