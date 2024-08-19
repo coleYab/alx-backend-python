fbbcebcdf95ec0582410aa480f37aa42b5947c7d
coleYab <yabume123@gamil.com>
2024-08-19 16:57:41 +0300

    + created first_task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Async: abd await for the program
+"""
+
+
+import asyncio, random
+
+
+async def wait_random(max_delay: int=10) -> float:
+    """
+    wait_random - function to wait till the random time.
+    args:
+        - max_delay - maximum delay for the system.
+    """
+    rand_sec = random.random() * max_delay
+    await asyncio.sleep(rand_sec)
+    return rand_sec
